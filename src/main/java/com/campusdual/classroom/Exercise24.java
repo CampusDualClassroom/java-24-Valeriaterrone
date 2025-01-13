ccac2d7882080578a2e714a76c9006be11340344
valeriaterrone <valeriaprogramacioncurso@gmail.com>
2025-01-13 13:27:39 +0100

    ejercicio listo y test pasados correctamente.

--- src/main/java/com/campusdual/classroom/Exercise24.java
+++ src/main/java/com/campusdual/classroom/Exercise24.java
@@ -5,14 +5,23 @@ import java.util.Queue;
 
 public class Exercise24 {
     public static Queue<String> createQueue() {
-
+        Queue<String> queue = new LinkedList<>();
+        queue.add("Smith");
+        queue.add("Montessori");
+        queue.add("Peralta");
+        queue.add("House");
+        return queue;
     }
     public static void printAndEmptyQueue(Queue<String> queue) {
-
+        System.out.println("Elemento en la cima de la cola: " + queue.peek());
+        while (!queue.isEmpty()) {
+            System.out.println("Elemento eliminado: " + queue.poll());
+        }
     }
 
     public static void main(String[] args) {
-
+        Queue<String> queue = createQueue();
+        printAndEmptyQueue(queue);
     }
 
 }

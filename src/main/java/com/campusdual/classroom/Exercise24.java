0a822e74dfdbb82ee01cd70a49467325aec7da85
valeriaterrone <valeriaprogramacioncurso@gmail.com>
2025-01-31 12:31:20 +0100

    refactorizado y pasado los test!!

--- src/main/java/com/campusdual/classroom/Exercise24.java
+++ src/main/java/com/campusdual/classroom/Exercise24.java
@@ -1,27 +1,21 @@
 package com.campusdual.classroom;
 
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class Exercise24 {
     public static Queue<String> createQueue() {
-        Queue<String> queue = new LinkedList<>();
-        queue.add("Smith");
-        queue.add("Montessori");
-        queue.add("Peralta");
-        queue.add("House");
-        return queue;
+        return new LinkedList<>(List.of("Smith", "Montessori", "Peralta", "House"));
     }
+
     public static void printAndEmptyQueue(Queue<String> queue) {
-        System.out.println("Elemento en la cima de la cola: " + queue.peek());
-        while (!queue.isEmpty()) {
-            System.out.println("Elemento eliminado: " + queue.poll());
-        }
+       while (!queue.isEmpty()){
+           System.out.println(queue.remove());
+       }
     }
 
     public static void main(String[] args) {
-        Queue<String> queue = createQueue();
-        printAndEmptyQueue(queue);
+        printAndEmptyQueue(createQueue());
     }
-
 }
